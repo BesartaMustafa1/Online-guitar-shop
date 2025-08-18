4b97d4d5cb9fe31da15407542668a84b01b26b1d
Besarta Mustafa <116516097+BesartaMustafa1@users.noreply.github.com>
2025-08-18 19:13:58 +0200

    Update apollo-client.ts
    Fixed the link

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'https://YOUR-GQL-ENDPOINT/graphql',
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'https://graphql-api-brown.vercel.app/api/graphql',
   }),
   cache: new InMemoryCache(),
 });
